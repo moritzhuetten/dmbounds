4376bc8f5b854b33ee3ea3a1484ad86131d20a71
Moritz Hütten <huetten@icrr.u-tokyo.ac.jp>
2022-02-25 18:23:05 +0900

    plotting script with interactive select

--- code/aux.py
+++ code/aux.py
@@ -26,5 +26,30 @@ def make_grid(xmin, xmax, npoints=300, unit='TeV', log=True):
         x_grid = np.linspace(xmin_val, xmax_val, npoints)
     return (x_grid *u.TeV).to(unit)
 
+def table_to_dict(table, keycolumn_name, valuecolumn_name):
+    dict = {}
+    for column in range(len(table)):
+        dict[table[keycolumn_name][column]] = table[valuecolumn_name][column]
+    return dict
+
+def get_key_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
+
+def get_names_str(name_tuple, name_dict):
+    if isinstance(name_tuple,str):
+        return name_dict[name_tuple]
+    elif isinstance(name_tuple,list):
+        namestring = ''
+        for name in name_tuple:
+            if name.isdigit():
+                namestring = namestring[:-1]
+                namestring += (' (' + name + ')/')
+            else:
+                namestring += (name_dict[name] + '/')
+        namestring = namestring[:-1]
+        return namestring
+    else:
+        print("Error")
+
 #def read_files():
     
